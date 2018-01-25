a5a434e7d910aaac03d0ff5e1581531fc85b8333
Renka <arifinrobert2012@gmail.com>
2018-01-25 23:32:20 +0700

    add facebook integration

--- app/src/main/java/com/apps/synclogin/syncloginapp/LoginActivity.java
+++ app/src/main/java/com/apps/synclogin/syncloginapp/LoginActivity.java
@@ -1,50 +1,57 @@
 package com.apps.synclogin.syncloginapp;
 
-import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Button;
 
 import com.apps.synclogin.syncloginapp.db.SQLiteHelper;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.Profile;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.Api;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.PendingResult;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
 
-import java.io.FileDescriptor;
-import java.io.PrintWriter;
-import java.util.concurrent.TimeUnit;
+import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity implements  View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
 
-    private Button signIn;
+    private Button googleSignIn, fbSignIn;
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
     private Button signOut;
     private SignInButton SignIn;
     private GoogleApiClient googleApiClient;
     private static final int REQ_CODE = 400;
+    private static final String EMAIL = "email";
+
+    @Override
+    public View onCreateView(String name, Context context, AttributeSet attrs) {
+        return super.onCreateView(name, context, attrs);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        signIn = findViewById(R.id.googleLoginBtn);
-        signOut = findViewById(R.id.twitterLoginBtn);
-        signOut.setOnClickListener(this);
-        signIn.setOnClickListener(this);
-
+        googleSignIn = findViewById(R.id.googleLoginBtn);
+        fbSignIn = findViewById(R.id.fbLoginBtn);
+        fbSignIn.setOnClickListener(this);
+        googleSignIn.setOnClickListener(this);
+        settingUpFBLogin();
         GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.
                 DEFAULT_SIGN_IN).requestEmail().build();
         googleApiClient = new GoogleApiClient.Builder(this).
@@ -56,8 +63,43 @@ public class LoginActivity extends AppCompatActivity implements  View.OnClickLis
         switch (v.getId()) {
             case R.id.googleLoginBtn:
                 signInWithGoogle();
+            case R.id.fbLoginBtn:
+                LoginManager.getInstance().logInWithReadPermissions(
+                        this, Arrays.asList("public_profile", "email", "user_friends")
+                );
                 break;
         }
+    }
+    private void settingUpFBLogin() {
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                if (loginResult != null) {
+                    Profile profile = Profile.getCurrentProfile();
+                    String name = profile.getName();
+                    String id = profile.getId();
+
+                    Intent i = new  Intent(getApplicationContext(), ProfileActivity.class);
+                    i.putExtra("name", name);
+                    i.putExtra("id", id);
+                    i.putExtra("loginType", SQLiteHelper.COLUMN_FB_ID);
+                    startActivity(i);
+                }
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
     }
     public void signInWithGoogle() {
         Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
@@ -71,15 +113,14 @@ public class LoginActivity extends AppCompatActivity implements  View.OnClickLis
 //        });
 //    }
 
+
     public void handleResult(GoogleSignInResult result) {
         if (result.isSuccess()) {
             GoogleSignInAccount account = result.getSignInAccount();
             String name = account.getDisplayName();
-            String email = account.getEmail();
             String id = account.getId();
             Intent i = new  Intent(getApplicationContext(), ProfileActivity.class);
             i.putExtra("name", name);
-            i.putExtra("email", email);
             i.putExtra("id", id);
             i.putExtra("loginType", SQLiteHelper.COLUMN_GOOGLE_ID);
             startActivity(i);
@@ -88,6 +129,7 @@ public class LoginActivity extends AppCompatActivity implements  View.OnClickLis
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
 
         if(requestCode == REQ_CODE) {
